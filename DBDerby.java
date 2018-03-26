7f824aa95862c877013ea27146cf8a3d009d4ea7
liam_ryan <s3606826@student.rmit.edu.au>
2018-03-26 06:08:49 +0000

    successfully load csv into DB - need to restucture

--- DBDerby.java
+++ DBDerby.java
@@ -10,49 +10,29 @@ public class DBDerby
             String db_url = "jdbc:derby:DerbyDb;create=true";
             Connection connection = DriverManager.getConnection(db_url);
 
-            Statement createTable = connection.createStatement();
-            createTable.executeUpdate("create table Businesses("
-                               + "BN_NAME varchar(30), "
-                               + "BN_STATUS varchar(30), "
-                               + "BN_REG_DT varchar(30), "
-                               + "BN_CANCEL_DT varchar(30), "
-                               + "BN_RENEW_DT varchar(30), "
-                               + "BN_STATE_NUM varchar(30), "
-                               + "BN_STATE_OF_REG varchar(30), "
-                               + "BN_ABN varchar(30), "
-                               + "primary key(BN_ABN))");
-            System.out.println("table created");
+            Statement stmt = connection.createStatement();
+                stmt.executeUpdate("create table Businesses("
+                                   + "BN_NAME varchar(200), "
+                                   + "BN_STATUS varchar(100), "
+                                   + "BN_REG_DT varchar(100), "
+                                   + "BN_CANCEL_DT varchar(100), "
+                                   + "BN_RENEW_DT varchar(100), "
+                                   + "BN_STATE_NUM varchar(100), "
+                                   + "BN_STATE_OF_REG varchar(100), "
+                                   + "BN_ABN varchar(100))");
+                System.out.println("table created");
+            stmt.execute("CALL SYSCS_UTIL.SYSCS_IMPORT_TABLE(null, 'BUSINESSES', 'a.csv', '	', null, null, 0)");
 
-            Statement insert = connection.createStatement();
-
-            insert.executeUpdate("insert into Businesses ("
-                               + "BN_NAME, "
-                               + "BN_STATUS, "
-                               + "BN_REG_DT, "
-                               + "BN_CANCEL_DT, "
-                               + "BN_RENEW_DT, "
-                               + "BN_STATE_NUM, "
-                               + "BN_STATE_OF_REG, "
-                               + "BN_ABN) "
-                               + "values ("
-                               + "'Warby Wares', "
-                               + "'Deregistered', "
-                               + "'31/05/2015', "
-                               + "'12/10/2017', "
-                               + "'31/05/2017', "
-                               + "'', "
-                               + "'', "
-                               + "'48697696446')");
             System.out.println("insert complete");
             
-            Statement get_row = connection.createStatement();
-            ResultSet res = get_row.executeQuery("select * from Businesses");
+            ResultSet res = stmt.executeQuery("select * from Businesses");
 
             while (res.next())
             {
-                String name = res.getString("BN_NAME");
-                String abn = res.getString("BN_ABN");
-                System.out.println("Business name: " + name + "\nABN: " + abn);
+                String sor = res.getString("BN_STATE_OF_REG");
+                String snum = res.getString("BN_STATE_NUM");
+                if (sor != null || snum != null)
+                    System.out.println("sor: " + sor + " | snum: " + snum);
             }
         }
         catch (SQLException e)
